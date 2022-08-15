ca5d9c658f74c5885a56f89902299401b11155f7
heerucan <iwpow0307@gmail.com>
2022-08-16 02:39:06 +0900

    add: add WeatherAPIManager requestWeatherInfo 메서드

--- Weather/Weather/Source/Network/Manager/WeatherManager.swift
+++ Weather/Weather/Source/Network/Manager/WeatherManager.swift
@@ -5,6 +5,37 @@
 //  Created by heerucan on 2022/08/16.
 //
 
-import Foundation
+import UIKit
 
+import Alamofire
+import SwiftyJSON
 
+struct WeatherManager {
+    private init() { }
+    static let shared = WeatherManager()
+    typealias completion = (Weather) -> ()
+    
+    // MARK: - GET Current Weather Info
+    
+    func requestWeather(lat: Double, lon: Double, completion: @escaping completion) {
+        let url = EndPoint.weatherURL + "lat=\(lat)&lon=\(lon)&appid=" + APIKey.WEATHER
+        
+        AF.request(url, method: .get).validate(statusCode: 200..<400).responseData(queue: .global()) { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                let weather = Weather(icon: String.makeIconURL(json["weather"][0]["icon"].stringValue),
+                                      description: json["weather"][0]["description"].stringValue,
+                                      humidity: json["main"]["humidity"].intValue,
+                                      cloud: json["clouds"]["all"].intValue,
+                                      temp: json["main"]["temp"].doubleValue,
+                                      tempMax: json["main"]["temp_max"].doubleValue,
+                                      tempMin: json["main"]["temp_min"].doubleValue)
+                completion(weather)
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
